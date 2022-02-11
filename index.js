2a26db1c488605485e8984a6cb22f14e43a31c19
Trevor Blades <tdblades@gmail.com>
2022-02-11 12:59:51 -0800

    Use graphql playground

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import {ApolloServer, gql} from 'apollo-server';
-import {ApolloServerPluginLandingPageProductionDefault} from 'apollo-server-core';
+import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core';
 import {buildSubgraphSchema} from '@apollo/subgraph';
 import {readFileSync} from 'fs';
 import {resolvers} from './resolvers.js';
@@ -7,20 +7,10 @@ import {resolvers} from './resolvers.js';
 const typeDefs = gql(readFileSync('./schema.graphql', 'utf-8'));
 const schema = buildSubgraphSchema({typeDefs, resolvers});
 
-let plugins;
-if (process.env.NODE_ENV === 'production') {
-  plugins = [
-    ApolloServerPluginLandingPageProductionDefault({
-      footer: false,
-      graphRef: process.env.APOLLO_GRAPH_REF
-    })
-  ];
-}
-
 const server = new ApolloServer({
   schema,
   introspection: true,
-  plugins
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()]
 });
 
 server.listen({port: process.env.PORT || 4000}).then(({url}) => {
